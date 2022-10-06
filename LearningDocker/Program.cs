fd346eb4ccc46fa482ed260fbdd6a128f9e4aa5f
Arnaud Vinsard <avinsard@gmail.com>
2022-10-06 17:06:41 +0200

    docker training

---
+++ LearningDocker/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+//WebApplication class : it used to configure the HTTP pipeline and routes
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ LearningDocker/README.md
@@ -0,0 +1,12 @@
+At the moment from the docker
+http:localhost:5000/weatherForecast
+
+Swagger endpoint doesn't work. Should I configure something on the dockerfile?
+
+Documentation 
+https://docs.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis?view=aspnetcore-6.0
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-6.0&tabs=visual-studio
+
+History:
+dotnet new webapi
+Add the dockerfile
