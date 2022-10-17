5584d8573b37b68343d0a4f9b13c065dbd77a63e
Arnaud Vinsard <avinsard@gmail.com>
2022-10-17 12:00:59 +0200

    added kata8

---
+++ Kata/Kata_8_Algo/Refactoring/CodingKata.Tests/MoneyPotServiceTests.cs
@@ -0,0 +1,14 @@
+using CodingKata.MoneyPots;
+using NUnit.Framework;
+
+namespace CodingKata.Tests
+{
+    public class MoneyPotServiceTests
+    {
+        [Test]
+        public async void TestSomething()
+        {
+            //TODO
+        }
+    }
+}
