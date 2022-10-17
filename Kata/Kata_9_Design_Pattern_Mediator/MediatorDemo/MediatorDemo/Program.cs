d1dd7a2f69c3bf1c422cff6a0e3edb51dea8f4e9
Arnaud Vinsard <avinsard@gmail.com>
2022-10-17 14:18:31 +0200

    Added examples mediator pattern

---
+++ Kata/Kata_9_Design_Pattern_Mediator/MediatorDemo/MediatorDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using MediatorDemo.ChatApp;
+using MediatorDemo.Structural;
+using System;
+
+namespace MediatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teamChat = new TeamChatroom();
+
+            var steve = new Developer("Steve");
+            var justin = new Developer("Justin");
+            var jenna = new Developer("Jenna");
+            var kim = new Tester("Kim");
+            var julia = new Tester("Julia");
+            teamChat.RegisterMembers(steve, justin, jenna, kim, julia);
+
+            steve.Send("Hey everyone, we're going to be deploying at 2pm today.");
+            kim.Send("Ok, thanks for letting us know.");
+
+            Console.WriteLine();
+            steve.SendTo<Developer>("Make sure to execute your unit tests before checking in!");
+        }
+
+
+
+        
+    }
+}
