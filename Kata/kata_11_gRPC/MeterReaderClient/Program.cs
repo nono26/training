22e391ac595780418c22e4c3e2e9fef28590b3cf
Arnaud Vinsard <arnaud.vinsard@xpollens.com>
2023-04-26 09:29:27 +0200

    added grpc worker client

---
+++ Kata/kata_11_gRPC/MeterReaderClient/Program.cs
@@ -0,0 +1,10 @@
+using MeterReaderClient;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
